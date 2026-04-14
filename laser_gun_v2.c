938a0e7127098f5153f4cf747aa276582fcaef49
huangkim192 <huangkim@seas.upenn.edu>
2026-04-14 16:18:06 -0400

    no finalized code for v2

--- laser_gun_v2.c
+++ laser_gun_v2.c
@@ -1,6 +1,6 @@
 /*
-Blaster V2 
-This code integrates the SPI LCD into the code. 
+Blaster V2
+this code integrates the SPI LCD into the code
 
 lcd requirements for current MVP:
 - display ammo count on startup
@@ -13,135 +13,193 @@ lcd requirements for current MVP:
 
 possible future lcd additions:
 - reload progress indicator
-
 */
-
-
 
 #define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <stdlib.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 
-// button stuff 
-#define BTN_NONE    0
-#define BTN_TRIGGER 1   // PD2
-#define BTN_RELOAD  2   // PD3
-// this variable is our flag for the interrupt 
-volatile uint8_t button_event = 0;
-
-
-// graphics
 #include "ST7735.h"
 #include "LCD_GFX.h"
 
-#define SCREEN_W        160
-#define SCREEN_H        128
+// button stuff
+#define BTN_NONE    0
+#define BTN_TRIGGER 1
+#define BTN_RELOAD  2
 
-// ammo count 
-uint8_t ammo = 12; 
+// move IR LED off PB2 because PB2 is used by LCD chip select
+#define IR_LED_DDR   DDRC
+#define IR_LED_PORT  PORTC
+#define IR_LED_PIN   PORTC0
 
+#define MAX_AMMO 12
 
-void initial_LCD_screen(void){
-    // need an additional function to refresh based on changes to the states? 
-    char buf[20];
-    snprintf(buf, sizeof(buf), "Ammo: %d", ammo);
-    LCD_drawString(10, 4, buf, WHITE, RED);
-}
+volatile uint8_t button_event = BTN_NONE;
+volatile uint8_t ammo = MAX_AMMO;
+volatile uint8_t reloading = 0;
+volatile uint8_t can_fire = 1;
 
-void trigger_action(void) {
-    // IR LED ON
-    PORTB |= (1 << PORTB2);
-    // some sort of delay? we dont want people to spam shooting, but delay_ms stops the entire microcontroller? 
-    _delay_ms(1000);
-    
-
-
-    // decrement ammo 
-    ammo--;
-}
-
-void reload_action(void) {
-    // clears the screen
-
-    // shows big text saying "reloading"
+// functions
+void gpio_init(void);
+void lcd_draw_status(const char *status_text);
+void initial_LCD_screen(void);
+void trigger_action(void);
+void reload_action(void);
+void fire_ir_pulse(void);
 
 
-}
-
-int main(void) {
-    // SETUP 
-    
-    // LCD setup
+int main(void)
+{
     lcd_init();
-
-
-    // 1. button setup 
-    DDRD &= ~(1 << DDD3); 
-    DDRD &= ~(1<< DDD2); 
-    
-    // enable internal pullups, buttons will be on gnd 
-    PORTD |= (1 << PORTD2);
-    PORTD |= (1 << PORTD3);
-    
-    // enable pci for port d
-    PCICR |= (1 << PCIE2);
-    
-    // enable intterupts on PD3 and PD2 
-    PCMSK2 |= (1<<PCINT18) | (1<<PCINT19);
-    
+    gpio_init();
     sei();
-    
 
-    // 2. IR led setup 
-    // Set PB2 as output
-    DDRB |= (1 << DDB2);
+    initial_LCD_screen();
 
-    // Start OFF 
-    PORTB &= ~(1 << PORTB2);
-    
-    
-    // MAIN LOOP
-    while (1) {
-        
-        // check the button events
-        if (button_event == BTN_TRIGGER) {
-            button_event = NONE;
-            // first make a check if there's enough ammo 
-            if (ammo > 0) {
-                // do trigger action 
+    while (1)
+    {
+        if (button_event == BTN_TRIGGER)
+        {
+            button_event = BTN_NONE;
+
+            if (reloading)
+            {
+                lcd_draw_status("RELOADING");
+            }
+            else if (!can_fire)
+            {
+                lcd_draw_status("COOLDOWN");
+            }
+            else if (ammo > 0)
+            {
                 trigger_action();
             }
-            else {
-                // NO AMMO! update on the LCD indicating no ammo / EMPTY 
-
+            else
+            {
+                lcd_draw_status("EMPTY");
             }
         }
-        else if (button_event == BTN_RELOAD) {
+        else if (button_event == BTN_RELOAD)
+        {
             button_event = BTN_NONE;
-            // do reload action
-            reload_action();
+
+            if (!reloading)
+            {
+                reload_action();
+            }
         }
     }
 }
 
+void gpio_init(void)
+{
+    // buttons on PD2 and PD3 as inputs
+    DDRD &= ~(1 << DDD2);
+    DDRD &= ~(1 << DDD3);
 
-// the interrupts sets the flag events 
+    // internal pullups enabled
+    PORTD |= (1 << PORTD2);
+    PORTD |= (1 << PORTD3);
+
+    // pin change interrupt for port D
+    PCICR |= (1 << PCIE2);
+    PCMSK2 |= (1 << PCINT18) | (1 << PCINT19);
+
+    // IR LED output on PC0
+    IR_LED_DDR |= (1 << DDC0);
+    IR_LED_PORT &= ~(1 << IR_LED_PIN);
+}
+
+void initial_LCD_screen(void)
+{
+    // displays Ammo count and the status of the blaster, currently in READY state  
+    lcd_draw_status("READY");
+}
+
+void lcd_draw_status(const char *status_text)
+{
+    char buf[20];
+
+    LCD_setScreen(BLACK);
+
+    snprintf(buf, sizeof(buf), "Ammo: %u", ammo);
+    LCD_drawString(10, 10, buf, WHITE, BLACK);
+
+    LCD_drawString(10, 30, "Status:", CYAN, BLACK);
+    LCD_drawString(10, 45, (char *)status_text, YELLOW, BLACK);
+}
+
+void fire_ir_pulse(void)
+{
+    // placeholder for now
+    // later this should become the 38kHz modulated burst
+    IR_LED_PORT |= (1 << IR_LED_PIN);
+    _delay_ms(80);
+    IR_LED_PORT &= ~(1 << IR_LED_PIN);
+}
+
+void trigger_action(void)
+{
+    can_fire = 0;
+
+    fire_ir_pulse();
+
+    if (ammo > 0)
+    {
+        ammo--;
+    }
+
+    if (ammo == 0)
+    {
+        lcd_draw_status("EMPTY");
+    }
+    else
+    {
+        lcd_draw_status("FIRED");
+    }
+
+    // simple MVP cooldown
+    _delay_ms(500);
+    can_fire = 1;
+
+    if (ammo > 0)
+    {
+        lcd_draw_status("READY");
+    }
+    else
+    {
+        lcd_draw_status("EMPTY");
+    }
+}
+
+void reload_action(void)
+{
+    reloading = 1;
+    lcd_draw_status("RELOADING");
+
+    // placeholder reload behavior
+    // proposal says hold-to-reload is 10s, but this is fine for current LCD bringup
+    _delay_ms(1500);
+
+    ammo = MAX_AMMO;
+    reloading = 0;
+    can_fire = 1;
+
+    lcd_draw_status("READY");
+}
+
 ISR(PCINT2_vect)
 {
-    // PD2 (trigger)
-    if (!(PIND & (1 << PIND2))) {
-        // PD2 went LOW
+    if (!(PIND & (1 << PIND2)))
+    {
         button_event = BTN_TRIGGER;
     }
-    
-    // PD3 (reload)
-    if (!(PIND & (1 << PIND3))) {
-        // PD3 went LOW
+    else if (!(PIND & (1 << PIND3)))
+    {
         button_event = BTN_RELOAD;
     }
 }
